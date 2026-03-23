01dbe94fa968ab2400fbbe8b6ab127b20bca2c2f
Simon Masson <simon.masson@protonmail.com>
2026-03-23 12:25:17 +0100

    cleaner account

--- packages/pq-account/src/ZKNOX_ERC4337_account.sol
+++ packages/pq-account/src/ZKNOX_ERC4337_account.sol
@@ -34,43 +34,6 @@ contract ZKNOX_ERC4337_account is BaseAccount {
         return _entryPoint;
     }
 
-    /// @notice Verify hybrid signature (pre- and post-quantum)
-    /// @param preQPubKey is a pre-quantum public key
-    /// @param postQPubKey is a post-quantum public key
-    /// @param preQLogicContractAddress the logic of the pre-quantum verification
-    /// @param postQLogicContractAddress the logic of the post-quantum verification
-    /// @param digest The data that was signed
-    /// @param preQuantumSig the pre-quantum signature: [r, s, v] for k1, [r, s] for r1
-    /// @param postQuantumSig the post-quantum signature (depending on the scheme)
-    /// @return true if both signatures are valid
-    function isValid(
-        bytes memory preQPubKey,
-        bytes memory postQPubKey,
-        address preQLogicContractAddress,
-        address postQLogicContractAddress,
-        bytes32 digest,
-        bytes memory preQuantumSig,
-        bytes memory postQuantumSig
-    ) public view returns (bool) {
-        // Validate digest length
-        if (digest.length > 32) {
-            return false;
-        }
-
-        // Verify pre-quantum signature
-        ISigVerifier preQuantumCore = ISigVerifier(preQLogicContractAddress);
-        if (preQuantumCore.verify(preQPubKey, digest, preQuantumSig) != preQuantumCore.verify.selector) {
-            return false;
-        }
-
-        // Verify post-quantum signature
-        ISigVerifier postQuantumCore = ISigVerifier(postQLogicContractAddress);
-        if (postQuantumCore.verify(postQPubKey, digest, postQuantumSig) != postQuantumCore.verify.selector) {
-            return false;
-        }
-        return true;
-    }
-
     /// @inheritdoc BaseAccount
     function _validateSignature(PackedUserOperation calldata userOp, bytes32 userOpHash)
         internal
@@ -79,18 +42,19 @@ contract ZKNOX_ERC4337_account is BaseAccount {
         returns (uint256 validationData)
     {
         (bytes memory preQuantumSig, bytes memory postQuantumSig) = abi.decode(userOp.signature, (bytes, bytes));
-        bool result = isValid(
-            preQuantumPubKey,
-            postQuantumPubKey,
-            preQuantumLogicContractAddress,
-            postQuantumLogicContractAddress,
-            userOpHash,
-            preQuantumSig,
-            postQuantumSig
-        );
-        if (!result) {
+
+        // Verify pre-quantum signature
+        ISigVerifier preQuantumCore = ISigVerifier(preQuantumLogicContractAddress);
+        if (preQuantumCore.verify(preQuantumPubKey, userOpHash, preQuantumSig) != preQuantumCore.verify.selector) {
             return SIG_VALIDATION_FAILED;
         }
+
+        // Verify post-quantum signature
+        ISigVerifier postQuantumCore = ISigVerifier(postQuantumLogicContractAddress);
+        if (postQuantumCore.verify(postQuantumPubKey, userOpHash, postQuantumSig) != postQuantumCore.verify.selector) {
+            return SIG_VALIDATION_FAILED;
+        }
+
         return SIG_VALIDATION_SUCCESS;
     }
 
